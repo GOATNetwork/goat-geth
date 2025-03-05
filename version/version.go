7481d03fd5c0bbc6fe3bfa80523036ecdde5d43b
ericlee <eric.l@metis.io>
2025-03-05 09:20:13 +0800

    version: goat 0.2.2 for mainnet

--- version/version.go
+++ version/version.go
@@ -17,8 +17,8 @@
 package version
 
 const (
-	Major = 0                // Major version component of the current release
-	Minor = 2                // Minor version component of the current release
-	Patch = 0                // Patch version component of the current release
-	Meta  = "stable-1.14.12" // Version metadata to append to the version string
+	Major = 0      // Major version component of the current release
+	Minor = 2      // Minor version component of the current release
+	Patch = 2      // Patch version component of the current release
+	Meta  = "goat" // Version metadata to append to the version string
 )
