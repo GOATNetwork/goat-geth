21042f5e2a0c5aef7e007e1bdb9a9140ec37ec77
ericlee <eric.l@metis.io>
2025-03-23 10:43:54 +0800

    config: update gas price settings for legacypool and miner

--- eth/ethconfig/goat.go
+++ eth/ethconfig/goat.go
@@ -8,12 +8,14 @@ import (
 	"github.com/ethereum/go-ethereum/miner"
 )
 
+const minGasPrice = 5000000
+
 var legacypoolConfig = legacypool.Config{
 	Journal:   "transactions.rlp",
 	Rejournal: time.Hour,
 
 	NoLocals:   true,
-	PriceLimit: 1e5,
+	PriceLimit: minGasPrice,
 	PriceBump:  10,
 
 	AccountSlots: 16,
@@ -21,13 +23,13 @@ var legacypoolConfig = legacypool.Config{
 	AccountQueue: 64,
 	GlobalQueue:  1024,
 
-	Lifetime: 10 * time.Minute,
+	Lifetime: 3 * time.Minute,
 }
 
 var minerConfig = miner.Config{
 	GasCeil:  30_000_000,
-	GasPrice: big.NewInt(1e5),
+	GasPrice: big.NewInt(minGasPrice),
 	Recommit: time.Second,
 }
 
-var maxGPOGasPrice = new(big.Int).SetUint64(legacypoolConfig.PriceLimit)
+var maxGPOGasPrice = big.NewInt(minGasPrice)
