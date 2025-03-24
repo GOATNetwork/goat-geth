0658cb0f553ac248dcbc868e1c729feb116e2fa5
ericlee <eric.l@metis.io>
2025-03-24 08:47:20 +0800

    version: 0.3.0

--- version/version.go
+++ version/version.go
@@ -20,5 +20,5 @@ const (
 	Major = 0      // Major version component of the current release
 	Minor = 3      // Minor version component of the current release
 	Patch = 0      // Patch version component of the current release
-	Meta  = "beta" // Version metadata to append to the version string
+	Meta  = "goat" // Version metadata to append to the version string
 )
