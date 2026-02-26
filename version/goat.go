065c5217bc8bba2772123a6422411054c0e522a9
ericlee <eric.l@metis.io>
2026-02-26 09:06:49 +0800

    version: v0.1.11

--- version/goat.go
+++ version/goat.go
@@ -5,6 +5,6 @@ package version
 const (
 	Major = 0          // Major version component of the current release
 	Minor = 1          // Minor version component of the current release
-	Patch = 10         // Patch version component of the current release
+	Patch = 11         // Patch version component of the current release
 	Meta  = "testnet3" // Version metadata to append to the version string
 )
