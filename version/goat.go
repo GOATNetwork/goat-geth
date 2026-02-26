ad61b188dceec39b69785962d3cbb58143632361
ericlee <eric.l@metis.io>
2026-02-26 09:00:44 +0800

    version: v0.4.1

--- version/goat.go
+++ version/goat.go
@@ -5,6 +5,6 @@ package version
 const (
 	Major = 0      // Major version component of the current release
 	Minor = 4      // Minor version component of the current release
-	Patch = 0      // Patch version component of the current release
+	Patch = 1      // Patch version component of the current release
 	Meta  = "goat" // Version metadata to append to the version string
 )
